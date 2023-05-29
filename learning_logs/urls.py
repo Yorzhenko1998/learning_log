e7ae667e3292ab365aa4f22f6fcc8361b94ab4fd
Yorzhenko1998 <ojorzhenko@gmail.com>
2023-05-29 18:52:06 +0200

    Added user functionality and the ability to delete themes.

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -18,4 +18,6 @@ urlpatterns = [
 	path('new_entry/<int:topic_id>/', views.new_entry, name='new_entry'),
 	# Page for edit entry.
 	path('edit_entry/<int:entry_id>/', views.edit_entry, name='edit_entry'),
+	# Delete a topic
+	path('topics/<int:topic_id>/delete/', views.delete_topic, name='delete_topic'),
 ]
--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render, redirect
-from django.template.context_processors import request
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth.decorators import login_required
+from django.http import Http404
 
 from .models import Topic, Entry
 from .forms import TopicForm, EntryForm
@@ -8,19 +9,25 @@ def index(request):
 	"""Main page "Журнал спостережень"."""
 	return render(request, 'learning_logs/index.html')
 
+@login_required
 def topics(request):
 	"""Displays all topics."""
-	topics = Topic.objects.order_by('date_added')
+	topics = Topic.objects.filter(owner=request.user).order_by('date_added')
 	context = {'topics': topics}
 	return render(request, 'learning_logs/topics.html', context)
 
+@login_required
 def topic(request, topic_id):
 	"""Show a single topic all its entries."""
 	topic = Topic.objects.get(id=topic_id)
+	# Make sure the topic belongs to the current user.
+	check_topic_owner(topic, request.user)
+
 	entries = topic.entry_set.order_by('-date_added')
 	context = {'topic': topic, 'entries': entries}
 	return render(request, 'learning_logs/topic.html', context)
 
+@login_required
 def new_topic(request):
 	"""Add a new topic."""
 	if request.method != 'POST':
@@ -30,13 +37,16 @@ def new_topic(request):
 		# POST sent; process the data.
 		form = TopicForm(data=request.POST)
 		if form.is_valid():
-			form.save()
+			new_topic = form.save(commit=False)
+			new_topic.owner = request.user
+			new_topic.save()
 			return redirect('learning_logs:topics')
 
 	# Show empty or invalid form.
 	context = {'form': form}
 	return render(request, 'learning_logs/new_topic.html', context)
 
+@login_required
 def new_entry(request, topic_id):
 	"""Add a new entry for a particular topic."""
 	topic  = Topic.objects.get(id=topic_id)
@@ -56,10 +66,13 @@ def new_entry(request, topic_id):
 	context = {'topic': topic, 'form': form}
 	return render(request, 'learning_logs/new_entry.html', context)
 
+@login_required
 def edit_entry(request, entry_id):
 	"""Edit an existing entry."""
 	entry = Entry.objects.get(id=entry_id)
 	topic = entry.topic
+	if topic.owner != request.user:
+		raise Http404
 
 	if request.method != 'POST':
 		# Initial request; pre-fill form with the current entry.
@@ -73,3 +86,28 @@ def edit_entry(request, entry_id):
 
 	context = {'entry': entry, 'topic': topic, 'form': form}
 	return render(request, 'learning_logs/edit_entry.html', context)
+
+def check_topic_owner(topic, user):
+	"""Make sure the currently logged-in user owns the topic that's
+	    being requested.
+
+	    Raise Http404 error if the user does not own the topic.
+	"""
+	if topic.owner != user:
+		raise Http404
+
+@login_required
+def delete_topic(request, topic_id):
+	"""Get the topic object or return a 404 error if the topic does not exist."""
+	topic = Topic.objects.get(id=topic_id)
+
+	# We check whether the current user is the owner of the topic.
+	check_topic_owner(topic, request.user)
+
+	if request.method == 'POST':
+		# Delete topic.
+		topic.delete()
+		return redirect('learning_logs:topics')
+
+	context = {'topic': topic}
+	return render(request, 'learning_logs/delete_topic.html', context)
